c1d8de07e7ef177732cefb950185a7deef106ef4
Pitt Wu <kgb00128@gmail.com>
2020-10-10 22:04:43 +0800

    [feat]：initial docsify project

---
+++ docs/js/index.js
@@ -0,0 +1,10 @@
+window.$docsify = {
+  name: '',
+  repo: '',
+  // open coverpage
+  coverpage: true,
+  logo: '/_media/icon.svg',
+  name: 'docsify123',
+  // coverpage: ['/', '/zh-cn/'],
+};
+console.log(123);
